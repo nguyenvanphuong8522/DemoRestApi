5a07cdb1eb6154cfded423785b764443b4133c23
Nguyen Van Phuong <nguyenvanphuong8522@gmail.com>
2024-08-30 15:18:00 +0700

    feat: init project

---
+++ DemoRestApi3/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using DemoRestApi3.Models;
+using DemoRestApi3.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoRestApi3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return Ok(categories);
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> Post(Category category)
+        {
+            await _categoryService.CreateAsync(category);
+            return Ok("Create Successful");
+        }
+    }
+}
---
+++ DemoRestApi3/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace DemoRestApi3.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
