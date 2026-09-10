618019db08f03c9957f32e2b3ef31a7a50ebaf7c
nguyenvanphuong <nguyenvanphuong8522@gmail.com>
2026-09-10 22:47:57 +0700

    fix http https

--- SocketDemo/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ SocketDemo/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43bb368d538f168dfc123bc9e8fb62d0a64934ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
